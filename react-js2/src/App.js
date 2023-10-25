69adc66bebd15d4ac14971d7be07968b5fb972d2
u1yuvan <yuvanraj700@gmail.com>
2023-10-25 20:05:39 +0530

    Refs in react

--- react-js2/src/App.js
+++ react-js2/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Component1 from './Components/Component1';
+import RefsDemo from './Components/RefsDemo';
 
 function App() {
   return (<React.Fragment>
-           <h1>This is App Component</h1>
-           <Component1></Component1>
+           {/* <h1>This is App Component</h1>
+           <Component1></Component1> */}
+           <RefsDemo></RefsDemo>
   </React.Fragment>);
 }
 

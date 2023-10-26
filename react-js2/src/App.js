a4cafa5578168530fa475a1d0d17ed7f8da2e4ad
u1yuvan <yuvanraj700@gmail.com>
2023-10-26 14:18:44 +0530

    Understanding SideEffects in React

--- react-js2/src/App.js
+++ react-js2/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Component1 from './Components/Component1';
-import RefsDemo from './Components/RefsDemo';
 
 function App() {
-  return (<React.Fragment>
-           {/* <h1>This is App Component</h1>
-           <Component1></Component1> */}
-           <RefsDemo></RefsDemo>
-  </React.Fragment>);
+  console.log("App Component Rendered");
+let [resourseType, setResourseType] = useState('Home');
+
+useEffect(() => {
+  console.log(resourseType);
+}, [resourseType]);
+
+ return (
+  <div className='App'>
+      <button onClick={() => {setResourseType('Home')}}>HOME</button>
+      <button onClick={() => {setResourseType('About')}}>ABOUT</button>
+      <button onClick={() => {setResourseType('Contact')}}>CONTACT</button>
+      <h3>{resourseType}</h3>
+  </div>
+ );
 }
 
 export default App;
--- react-js2/src/Components/PortalComponent.js
+++
@@ -1,7 +0,0 @@
-import  ReactDOM  from "react-dom";
-
-function PortalComponent(){
-    return ReactDOM.createPortal(<h1>This is Portal Component</h1>, document.getElementById('root-portal')); 
-}
-
-export default  PortalComponent;

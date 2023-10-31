a516a1a9e49195a40deced73081faf6a8ec652d1
u1yuvan <yuvanraj700@gmail.com>
2023-10-31 13:02:10 +0530

    Using Sass on React

---
+++ react-js2/src/Components/GrowingPlant/GrowingPlant.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./GrowingPlant.scss"
+
+
+function GrowingPlant() {
+  return (
+    <div className='plant'>
+        <div className='text'>GrowingPlant</div>
+        <div className='logo'>This is my Plant 🌵</div>
+    </div>
+  )
+}
+
+export default GrowingPlant
---
+++ react-js2/src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import GrowingPlant from '../GrowingPlant/GrowingPlant'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Welcome to Home</h1>
+        <GrowingPlant/>
+
+    </div>
+  )
+}
+
+export default Home

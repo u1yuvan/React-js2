e9c2ec27093971b14f49c1ee1722c380275fbe23
u1yuvan <yuvanraj700@gmail.com>
2023-11-03 13:59:24 +0530

    Embedding Expressions

--- react-js2/src/Components/Products.js
+++ react-js2/src/Components/Products.js
@@ -1,12 +1,21 @@
+let productCount = 0;
+
+function displayFormattedProductCount(){
+    return productCount > 0 ? productCount: "0"
+}
+
 function Products() {
     return (<ul className="list-group shadow">
                 <li className="list-group-item">
-                    <div className="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
                         <div className="media-body order-2 order-lg-1">
                             <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
                             <p className="font-italic text-muted mb-0 small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
-                            <div className="d-flex align-items-center justify-content-between mt-1">
+                            <div className="d-flex align-items-center justify-content-start mt-1">
                                 <h6 className="font-weight-bold my-2">$120.00</h6>
+                                <button className="btn btn-primary">-</button>
+                                <span>{displayFormattedProductCount()}</span>
+                                <button className="btn btn-primary">+</button>
                             </div>
                         </div><img src="" alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2" />
                     </div>

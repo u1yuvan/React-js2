884691af4ff56fd69184726364bcb4e79c18e25a
u1yuvan <yuvanraj700@gmail.com>
2023-11-03 13:19:36 +0530

    Adding Child Component

---
+++ react-js2/src/Components/Products.js
@@ -0,0 +1,7 @@
+function Products(){
+    return <h1>All Products</h1>
+}
+
+
+
+export default Products;
---
+++ react-js2/src/app.js
@@ -0,0 +1,13 @@
+import './app.css'
+import Products from './Components/Products';
+
+function App(){
+// const p = document.createElement('p');
+// p.textContent = 'This is a Demo Paragraph!'
+// return p;
+let msg = 'Awesome'
+    return <Products></Products>;
+}
+
+
+export  default App;

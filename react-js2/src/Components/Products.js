b71636fd103b9b0730d51f4d3b0f5209addafcbc
u1yuvan <yuvanraj700@gmail.com>
2023-11-03 13:45:36 +0530

    Adding Bootstrap in React App

--- react-js2/src/Components/Products.js
+++ react-js2/src/Components/Products.js
@@ -1,41 +1,17 @@
 function Products() {
-    return (<ul>
-                <li>
-                    <div>
-                        <div>
-                            <h5>Fresh Milk</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
-                            <div>
-                                <h6>$120.00</h6>
+    return (<ul className="list-group shadow">
+                <li className="list-group-item">
+                    <div className="media align-items-lg-center flex-column flex-lg-row p-3">
+                        <div className="media-body order-2 order-lg-1">
+                            <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
+                            <p className="font-italic text-muted mb-0 small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
+                            <div className="d-flex align-items-center justify-content-between mt-1">
+                                <h6 className="font-weight-bold my-2">$120.00</h6>
                             </div>
-                        </div><img src="" width="200" />
+                        </div><img src="" alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2" />
                     </div>
                 </li>
-
-                <li>
-                    <div>
-                        <div>
-                            <h5>Fresh Milk</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
-                            <div>
-                                <h6>$120.00</h6>
-                            </div>
-                        </div><img src="" width="200" />
-                    </div>
-                </li>
-
-                <li>
-                    <div>
-                        <div>
-                            <h5>Fresh Milk</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
-                            <div>
-                                <h6>$120.00</h6>
-                            </div>
-                        </div><img src="" width="200" />
-                    </div>
-                </li>
-            </ul>)
+    </ul>)
 }
 
 
--- react-js2/src/app.js
+++ react-js2/src/app.js
@@ -1,13 +1,18 @@
 import './app.css'
 import Products from './Components/Products';
 
-function App(){
-// const p = document.createElement('p');
-// p.textContent = 'This is a Demo Paragraph!'
-// return p;
-let msg = 'Awesome'
-    return <Products></Products>;
+function App() {
+    // const p = document.createElement('p');
+    // p.textContent = 'This is a Demo Paragraph!'
+    // return p;
+    let msg = 'Awesome'
+    return (<div className='row'>
+        <div className='col-lg-8 mx-auto'>
+            <Products></Products>;
+        </div>
+
+    </div>)
 }
 
 
-export  default App;
+export default App;

b202dd9a3bda95dd36d68687539b090316af00c4
u1yuvan <yuvanraj700@gmail.com>
2023-11-03 13:31:28 +0530

    Working with Complex JSX

--- react-js2/src/Components/Products.js
+++ react-js2/src/Components/Products.js
@@ -1,5 +1,41 @@
-function Products(){
-    return <h1>All Products</h1>
+function Products() {
+    return (<ul>
+                <li>
+                    <div>
+                        <div>
+                            <h5>Fresh Milk</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
+                            <div>
+                                <h6>$120.00</h6>
+                            </div>
+                        </div><img src="" width="200" />
+                    </div>
+                </li>
+
+                <li>
+                    <div>
+                        <div>
+                            <h5>Fresh Milk</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
+                            <div>
+                                <h6>$120.00</h6>
+                            </div>
+                        </div><img src="" width="200" />
+                    </div>
+                </li>
+
+                <li>
+                    <div>
+                        <div>
+                            <h5>Fresh Milk</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
+                            <div>
+                                <h6>$120.00</h6>
+                            </div>
+                        </div><img src="" width="200" />
+                    </div>
+                </li>
+            </ul>)
 }
 
 

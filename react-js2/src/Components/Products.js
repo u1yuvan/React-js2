2be77e57c7b6ed0398417430f92477e80f96bcb0
u1yuvan <yuvanraj700@gmail.com>
2023-11-04 12:52:02 +0530

    Understanding child Props

--- react-js2/src/Components/Products.js
+++ react-js2/src/Components/Products.js
@@ -1,10 +1,5 @@
 import ProductDetails from "./ProductDetails";
 
-
-
-
-
-
 function Products(props) {
 
     return (
@@ -13,13 +8,13 @@ function Products(props) {
                         <div className="media-body order-2 order-lg-1">
                             <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
                             <p className="font-italic text-muted mb-0 small">{props.description}</p>
-                            <ProductDetails  price={props.price} isAvailable={props.isAvailable}/>
+                            <ProductDetails  price={props.price} isAvailable={props.isAvailable}>
+                                <p>This is a Demo Paragraph</p>
+                            </ProductDetails>
                         </div><img src={props.imageUrl}alt="Generic placeholder image" width="200" className="ml-lg-5 order-1 order-lg-2" />
                     </div>
                 </li>
  )
 }
-
-
 
 export default Products;

45a224418f2e8c972d71b0df317077cfc548880a
u1yuvan <yuvanraj700@gmail.com>
2023-11-08 12:37:50 +0530

    Handling Form Submission..

--- react-js2/src/Components/CreateProduct/ProductForm.js
+++ react-js2/src/Components/CreateProduct/ProductForm.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 
 function ProductForm() {
@@ -7,23 +7,36 @@ function ProductForm() {
     let [pDescription, updateDescription] = useState('');
     let [pAvailable, updateAvilability] = useState('');
     let [pImageUrl, updateImageUrl] = useState('');
-   
-    function nameInputHandler(event){
-        updateName(event.target.value);    
+
+    function nameInputHandler(event) {
+        updateName(event.target.value);
     }
-    function priceInputHandler(event){
-        updatePrice(event.target.value);    
+    function priceInputHandler(event) {
+        updatePrice(event.target.value);
     }
-    function descriptionInputHandler(event){
-        updateDescription(event.target.value);    
+    function descriptionInputHandler(event) {
+        updateDescription(event.target.value);
     }
-    function availabilityInputHandler(event){
-        updateAvilability(event.target.value);    
+    function availabilityInputHandler(event) {
+        updateAvilability(event.target.checked);
     }
-    function imageInputHandler(event){
-        updateImageUrl(event.target.value);    
+    function imageInputHandler(event) {
+        updateImageUrl(event.target.value);
     }
-    return (<form className="row g-3">
+
+    function createProductEventHandler(event) {
+        event.preventDefault();
+        let product = {
+            pID: 1,
+            pName: pName,
+            desc: pDescription,
+            isAvailable: pAvailable,
+            image: pImageUrl,
+            price: Number(pPrice),
+        }
+        console.log(product);
+    }
+    return (<form className="row g-3" onSubmit={createProductEventHandler}>
         <div className="col-md-6">
             <label for="name">Product Name</label>
             <input type="text"
@@ -53,14 +66,14 @@ function ProductForm() {
 
         <div class="form-check form-switch">
             <input className="form-check-input" type="checkbox" role="switch" id="isAvailable"
-            onChange={availabilityInputHandler} />
+                onChange={availabilityInputHandler} />
             <label className="form-check-label" for="isAvailable">Is product available in stock?</label>
         </div>
 
         <div className="form-group">
             <label for="select-image">Select product image</label>
             <input type="file" className="form-control" id="select-image"
-            onChange={imageInputHandler} />
+                onChange={imageInputHandler} />
         </div>
 
 
